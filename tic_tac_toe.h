ac303da75837997c80b9c595543338abe6a2cddc
El Mahdi Gaga <elmahdigaga9@gmail.com>
2023-02-14 23:30:27 +0100

    Added board class with constructor and destructor

--- tic_tac_toe.h
+++ tic_tac_toe.h
@@ -1,9 +1,40 @@
 #ifndef TIC_TAC_TOE_H
 #define TIC_TAC_TOE_H
 
-typedef struct
-{
+#define ROWS 3
+#define COLS 3
+
+typedef struct {
     char symbol;
 } Player;
+
+class Board {
+   private:
+    char** matrix;
+    int nbr_moves;
+    Player p1;
+    Player p2;
+
+   public:
+    Board();
+    ~Board();
+};
+
+Board::Board() {
+    matrix = new char*[ROWS];
+    for (short int i = 0; i < ROWS; ++i) {
+        matrix[i] = new char[COLS];
+    }
+    nbr_moves = 0;
+    p1.symbol = ' ';
+    p2.symbol = ' ';
+}
+
+Board::~Board() {
+    for (short int i = 0; i < ROWS; ++i) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
 
 #endif
